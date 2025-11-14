14bdda3c5724e9ef6c849a7bfe81e060e91132d6
Liana Tomecek <liana.tomecek@gmail.com>
2025-11-14 12:08:22 -0700

    added jingle bell midi files and removed old scripts

--- Assets/_Project/Code/Gameplay/Midi Parsing/MidiParser.cs
+++ Assets/_Project/Code/Gameplay/Midi Parsing/MidiParser.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Collections;
 using Melanchall.DryWetMidi.Multimedia;
+using System.Globalization;
 
 /*
     Using DryWetMidi Library plug-in
@@ -15,43 +16,58 @@ using Melanchall.DryWetMidi.Multimedia;
 
 
 
-public class NewMidiVisualizer : MonoBehaviour
+public class MidiParser : MonoBehaviour
 {
     [SerializeField] Object midiAsset;
+    private string _path;
+
     private MidiFile _midiFile;
     private List<TrackChunk> _tracks;
 
     private short _ticksPerQuarter;
     private float _microSecPerQuarter;
+    private const int MICROSECONDS_PER_SECOND = 1000000;
 
     void Start()
     {
         //Try to read the provided midi file
-        string path = AssetDatabase.GetAssetPath(midiAsset);
-        if (Path.GetExtension(path) != ".mid")
+        _path = AssetDatabase.GetAssetPath(midiAsset);
+        if (Path.GetExtension(_path) != ".mid")
         {
             throw new System.Exception("Invalid file extension (expected .mid)");
         }
 
+        LoadMidiInfo();
+
+        foreach (TrackChunk track in _tracks)
+        {
+            StartCoroutine(DisplayTrackNotes(track));
+        }
+    }
+    
+    private void LoadMidiInfo()
+    {
         // Load MIDI file and separate tracks
-        _midiFile = MidiFile.Read(path);
+        _midiFile = MidiFile.Read(_path);
         _tracks = _midiFile.GetTrackChunks().ToList();
 
+        Debug.Log($"Num of tracks:{_tracks.Count}");
+
+        //Read tempo-related information
         _microSecPerQuarter = _midiFile.GetTempoMap().GetTempoChanges().First().Value.MicrosecondsPerQuarterNote;    //assuming that tempo never changes for now
         _ticksPerQuarter = (_midiFile.TimeDivision as TicksPerQuarterNoteTimeDivision).TicksPerQuarterNote;
 
         Debug.Log($"ms/Quarter: {_microSecPerQuarter} || tick/Quarter {_ticksPerQuarter}");
 
-        foreach(TrackChunk track in _tracks)
-        {
-            StartCoroutine(DisplayTrackNotes(track));
-        }
     }
 
 
     public IEnumerator DisplayTrackNotes(TrackChunk track)
     {
-        float secondsPerTick = (_microSecPerQuarter / _ticksPerQuarter) / 1000000f;
+        if (track.GetNotes().Count == 0)       //if it not a track with notes
+            yield return 0;
+
+        float secondsPerTick = (_microSecPerQuarter / _ticksPerQuarter) / MICROSECONDS_PER_SECOND;
         Debug.Log($"Seconds per tick: {secondsPerTick}");
         long currentTime = 0;
         
